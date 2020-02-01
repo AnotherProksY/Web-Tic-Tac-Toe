ff18a628de5f2b19a9798c1c4cc6d80755a293b8
Kamil <k.fazilov@icloud.com>
2020-02-01 14:34:32 +0300

    Небольшие изменения в game.py и server.py

--- flaskr/server.py
+++ flaskr/server.py
@@ -1,11 +1,13 @@
-# Серверный модуль
-from flask import Flask 
+"""Серверный модуль"""
+
+# Модуль игровой логики
+import flaskr.game as GL
+
+from flask import Flask
 from flask import render_template
 from flask import request, redirect
 app = Flask(__name__)
 
-# Модуль игровой логики
-import flaskr.game as GL
 
 # Кто выиграл
 set_winner = ""
@@ -22,7 +24,8 @@ def main_page():
 @app.route('/GamePage')
 def game_page():
     """Страница с сеткой Tic-tac-toe"""
-    return render_template('game.html',
+    return render_template(
+                    'game.html',
                     place_1=str(GL.board[0]),
                     place_2=str(GL.board[1]),
                     place_3=str(GL.board[2]),
@@ -31,7 +34,8 @@ def game_page():
                     place_6=str(GL.board[5]),
                     place_7=str(GL.board[6]),
                     place_8=str(GL.board[7]),
-                    place_9=str(GL.board[8]))
+                    place_9=str(GL.board[8])
+    )
 
 
 @app.route('/GamePage/Score')
@@ -43,7 +47,7 @@ def game_page_score():
 
 
 # Функции----------------------------------------------------
-@app.route('/Turn', methods = ['POST'])
+@app.route('/Turn', methods=['POST'])
 def Turn():
     """Функция игровой петли"""
     # Функция проверки победителя
